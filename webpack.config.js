0cb5af95c6dd40b78019e183f8dc9db097b76144
Kevin Ji <kevin.ji@outlook.com>
2018-08-25 17:08:05 -0400

    Add framework for web-based code

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+export default {
+  entry: {
+    main: './web-src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'web-src', 'static'),
+    filename: '[name].js',
+  },
+  devtool: 'cheap-source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
