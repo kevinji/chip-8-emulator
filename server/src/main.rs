bacef4daec4611f7d21b938b9a27caee08fce53e
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-06-06 22:42:18 -0400

    server: Replace Python server with axum server

---
+++ server/src/main.rs
@@ -0,0 +1,52 @@
+use axum::Router;
+use clap::Parser;
+use http::header::{self, HeaderName, HeaderValue};
+use http::Method;
+use std::net::Ipv4Addr;
+use tower::ServiceBuilder;
+use tower_http::cors::{self, CorsLayer};
+use tower_http::services::ServeDir;
+use tower_http::ServiceBuilderExt;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Port to start the server on
+    #[clap(long, default_value_t = 3000)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let Args { port } = Args::try_parse()?;
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::OPTIONS])
+        .allow_origin(cors::Any);
+
+    let service_builder = ServiceBuilder::new()
+        .layer(cors)
+        .append_response_header(
+            HeaderName::from_static("cross-origin-embedder-policy"),
+            HeaderValue::from_static("require-corp"),
+        )
+        .append_response_header(
+            HeaderName::from_static("cross-origin-opener-policy"),
+            HeaderValue::from_static("same-origin"),
+        )
+        .append_response_header(
+            header::X_CONTENT_TYPE_OPTIONS,
+            HeaderValue::from_static("nosniff"),
+        )
+        .append_response_header(header::X_FRAME_OPTIONS, HeaderValue::from_static("DENY"))
+        .append_response_header(header::X_XSS_PROTECTION, HeaderValue::from_static("0"));
+
+    let router = Router::new()
+        .nest_service("/", ServeDir::new("web-src"))
+        .layer(service_builder);
+
+    let addr = (Ipv4Addr::LOCALHOST, port).into();
+    let server = axum::Server::try_bind(&addr)?.serve(router.into_make_service());
+
+    server.await?;
+    Ok(())
+}
